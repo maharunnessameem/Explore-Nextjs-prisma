2c6314ab01c80e16ed13ed2d71dbfc5e8a61a0b7
Maharun Nessa Meem <maharunnessameem86@gmail.com>
2023-10-02 16:36:08 +0600

    setup routing navbar activelink font

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,11 @@
 import Header from './components/Header'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700']
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
 
         <div className='max-w-4xl mx-auto px-5'>
           <Header></Header>
