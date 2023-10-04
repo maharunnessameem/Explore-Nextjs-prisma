f11d215874a8ee53ce638990647ecfe82c5ed8f3
Maharun Nessa Meem <maharunnessameem86@gmail.com>
2023-10-04 15:03:21 +0600

    multiple api

---
+++ app/posts/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+const error = () => {
+    return (
+        <div>
+            There is a data fetching error...
+        </div>
+    );
+};
+
+export default error;
--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -2,14 +2,35 @@ import Link from "next/link";
 
 async function getPostsData() {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    await new Promise((resolve) => setTimeout(resolve, 3000))//wait 3 second
+
+    return res.json();
+}
+
+async function getUsersData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+
+    await new Promise((resolve) => setTimeout(resolve, 3000))//wait 3 second
+
     return res.json();
 }
 
 const Posts = async () => {
-    const posts = await getPostsData();
+    // const posts = await getPostsData();
+    const [posts, users] = await Promise.all([getPostsData(), getUsersData()]);
     return (
         <div>
             <h1 className="text-4xl">Post Page</h1>
+            <h2 className="text-2xl">Users</h2>
+
+            {
+                users.map((user, index) => (
+                    <p key={index}>{user.name}</p>
+                ))
+            }
+
+
             <ul className="flex flex-col gap-5">
                 {
                     posts.map(post => (
